12c36073ee50d6df65fd973bf95a2f9ac26ac310
KristiyanHr <20118@uktc-bg.com>
2026-04-05 23:28:49 +0300

    feat: add User and Tournament entities

---
+++ src/main/java/com/fantasytennis/fantasy_tennis/model/User.java
@@ -0,0 +1,27 @@
+package com.fantasytennis.fantasy_tennis.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTIFY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private int totalPoints;
+}
