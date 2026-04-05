d322c857bfa77f1a16a3304b07b9fdda72ac3c89
KristiyanHr <20118@uktc-bg.com>
2026-04-05 23:17:56 +0300

    Player entity updates introduced

---
+++ src/main/java/com/fantasytennis/fantasy_tennis/model/Player.java
@@ -0,0 +1,27 @@
+package com.fantasytennis.fantasy_tennis.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "players")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTIFY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private int ranking;
+    
+    private String country;
+
+    private int cost;
+}
