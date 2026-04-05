3addef7545d564373764954a50b27ea6c97c5e89
KristiyanHr <20118@uktc-bg.com>
2026-04-05 23:05:53 +0300

    Initial commit: Fresh Spring Boot setup with JPA, Web, H2, and Lombok

---
+++ src/main/java/com/fantasytennis/fantasy_tennis/FantasyTennisApplication.java
@@ -0,0 +1,13 @@
+package com.fantasytennis.fantasy_tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyTennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyTennisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantasytennis/fantasy_tennis/FantasyTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasytennis.fantasy_tennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyTennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
